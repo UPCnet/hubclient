d9a9021c955d7541ea95217702a87a21c474ccdd
Carles Bruguera <carlesba@gmail.com>
2015-04-28 15:07:46 +0200

    Finish hubclient

--- hubclient/__init__.py
+++ hubclient/__init__.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from maxclient.client import get_max_info
+from maxclient.client import get_max_url_from_hub_domain
 from maxclient.rest import MaxClient
 from hubclient.routes import ROUTES
 
@@ -7,3 +9,34 @@ class HubClient(MaxClient):
     __routes__ = ROUTES
     __defaults__ = {}
 
+    def __init__(self, domain, hub, max_server=None, **kwargs):
+        self.__max_server__ = max_server
+        self.domain = domain
+        kwargs['oauth_server'] = None
+        super(HubClient, self).__init__(hub, **kwargs)
+
+    @property
+    def max_server(self):
+        if self.__max_server__ is None:
+            self.__max_server__ = get_max_url_from_hub_domain(self.url, self.domain)
+        return self.__max_server__
+
+    @property
+    def oauth_server(self):
+        if self.__oauth_server__ is None:
+            max_info = get_max_info(self.max_server)
+            self.__oauth_server__ = max_info['max.oauth_server']
+        return self.__oauth_server__.rstrip('/')
+
+    def OAuth2AuthHeaders(self):
+        """
+        """
+        headers = {
+
+            'X-Oauth-Token': str(self.token),
+            'X-Oauth-Username': str(self.actor['username']),
+            'X-Oauth-Scope': str(self.scope),
+            'X-Oauth-Domain': str(self.domain),
+
+        }
+        return headers
